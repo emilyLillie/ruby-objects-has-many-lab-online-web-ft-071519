06ce1023fe4516d2ed58f478bffbc6929b59d8a4
Emily Lillie <emilylillie3@gmail.com>
2019-07-29 13:50:51 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,27 +1,27 @@
-class Author 
-attr_accessor :name, :post  
-@count = 1 
+# class Author 
+# attr_accessor :name, :post  
+# @count = 1 
 
-def intialize(name)
-  @name = name 
-  @posts = [] 
-end
+# def intialize(name)
+#   @name = name 
+#   @posts = [] 
+# end
   
-def add_post(post) 
-  @count += 1
-  @posts << post 
-  post.author = self
-end
+# def add_post(post) 
+#   @count += 1
+#   @posts << post 
+#   post.author = self
+# end
 
-def add_post_by_title(title)
-  @count +=1 
-  post = Post.new(title)
-  @posts << post 
-  post.author = self 
-end
+# def add_post_by_title(title)
+#   @count +=1 
+#   post = Post.new(title)
+#   @posts << post 
+#   post.author = self 
+# end
 
-def self.post_count
-  @count
-end
+# def self.post_count
+#   @count
+# end
 
-end 
+# end 
