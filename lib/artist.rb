d018d477aa8a130aef7ffd5c0341033195fa486b
Emily Lillie <emilylillie3@gmail.com>
2019-07-29 14:22:47 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,60 +1,33 @@
-# class Artist
-# attr_accessor :name
-# @@count = 1
-
-#   def initialize(name)
-#     @name = name
-#     @@songs = []
-#   end
-
-#   def add_song(song)
-#     @@count += 1 
-#     @@songs << song
-#     song.artist = self
-#   end
-
-#   def songs
-#   @@songs
-#   end
-
-#   def add_song_by_name(name)
-#     @@count += 1 
-#     song = Song.new(name)
-#     @@songs << song
-#     song.artist = self
-#   end
-
-#   def self.song_count
-#   @@count
-#   end
-
-# end
-
-
 class Artist
+attr_accessor :name
+@@count = 1
 
- 	attr_accessor :name, :songs
+  def initialize(name)
+    @name = name
+    @@songs = []
+  end
 
- 	@@all = []
+  def add_song(song)
+    @@count += 1 
+    @@songs << song
+    song.artist = self
+  end
 
- 	def initialize(name)
-		@name = name
-		@songs = []
-		@@all << self
-	end
+  def songs
+  @@songs
+  end
 
- 	def add_song(song)
-		@songs << song
-		song.artist = self
-	end
+  def add_song_by_name(name)
+    @@count += 1 
+    song = Song.new(name)
+    @@songs << song
+    song.artist = self
+  end
 
- 	def add_song_by_name(song_name)
-		song = Song.new(song_name)
-		add_song(song)
-	end
+  def self.song_count
+  @@count
+  end
 
- 	def self.song_count
-		Song.all.length
-	end
+end
 
- end
+
