ea019b2965e9a5974fa1cfae9c2caee2637e7581
Emily Lillie <emilylillie3@gmail.com>
2019-07-29 13:25:58 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,31 @@
+class Artist
+attr_accessor :name
+@@count = 1
+
+  def initialize(name)
+    @name = name
+    @@songs = []
+  end
+
+  def add_song(song)
+    @@count += 1 
+    @@songs << song
+    song.artist = self
+  end
+
+  def songs
+   @@songs
+  end
+
+  def add_song_by_name(name)
+    @@count += 1 
+    song = Song.new(name)
+    @@songs << song
+    song.artist = self
+   end
+
+  def self.song_count
+   @@count
+  end
+
+end
